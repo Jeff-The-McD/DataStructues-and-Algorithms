ad3e70ab417b50d7b7c7073f9d5d6927ceae5a58
Shrine-Maiden-of-Paradise <jmcdonald@cpp.edu>
2020-07-06 10:57:06 -0700

    "finished all linkedlist"

--- Java_DataStruct/LinkedList.java
+++ Java_DataStruct/LinkedList.java
@@ -38,6 +38,26 @@ public class LinkedList
 		number_of_entries++;
 	 }
 	 
+	// obj: deletes the node at the end 
+	// args:none
+	// return value:returns nothing 
+	// time complexity: O(n) has to traverse the end of the list at the worse case
+	 public void deleteEnd()
+	 {
+		 if(head == null) {return;}
+		 		 
+		 if(head.next == null) {return;}
+		 
+		 Node tracker = head;
+		 
+		 while(tracker.next.next != null)
+		 {
+			 tracker = tracker.next;
+		 }
+		 tracker.next = null;
+		 number_of_entries--;
+	 }
+	 
 	 // obj: adds a new node at the head of the list
 	 // args : takes the data and creates a new node to add
 	 // return value : void
@@ -55,34 +75,84 @@ public class LinkedList
 		 number_of_entries++;
 	 }
 	 
-	 public void insertAt(int data, int index)
+	 // obj: deletes the head of the node
+	 // args: none
+	 // return value: void
+	 // time complexity : O(1) as the head is constant 
+	 public void deleteHead()
 	 {
-		 if(index>number_of_entries) 
-		 {
-			 System.out.println("index exceeded list size");
-			 return;
-		 }
-		 
-		 if(index==0) {addAtHead(data);}
-		 
-		 Node new_node = new Node(data);
-		 
 		 Node tracker = head;
-		 
-		 for(int i = 0; i<=number_of_entries;i++)
-		 {
-			 if(i == index)
-			 {
-				 new_node.next = tracker.next;
-				 tracker.next = new_node;
-				 
-			 }
-			 tracker = tracker.next;
-		 }
-		 number_of_entries++;
-		 
+		 tracker = tracker.next;
 	 }
 	 
+	 
+	// obj: insert at a specific position at the linked list
+	// args: data of the none ,  index 
+	// return value: void
+	// time complexity : O(n) as the head is constant 
+	 public void insertAt(int data, int index)
+	 {
+		 if(index<1) 
+		 {
+			 System.out.println("Invalid Position");
+			 return;
+		 } 
+		 
+		 if(index == 0)
+		 {
+			 addAtHead(data);
+		 }
+		 
+		 else
+		 {
+			 Node newNode = new Node(data);
+			 Node tracker = head;
+			 for(int i = 0;i<index-1;i++)
+			 {
+				 tracker = tracker.next;
+			 }
+			 
+			 Node nodeBefore = tracker;
+			 Node nodeAfter = nodeBefore.next;
+			 newNode.next = nodeAfter;
+			 nodeBefore.next = newNode;
+		 }
+	 }
+	 
+	 
+	 
+	 // obj: delete at a specific position
+	 // args: position to remove the node
+	 // return value: void
+	 // time complexity: O(n)
+	 public void deleteAt(int pos)
+	 {
+		 Node tracker = head;
+		 
+		 if(pos == 1)
+		 {
+			 head = head.next;
+		 }
+		 
+		 else
+		 {
+			 for(int i = 0;i<pos-1;i++)
+			 {
+				 tracker = tracker.next;
+			 }
+			 
+			 Node nodeBefore = tracker;
+			 Node nodeToRemove = nodeBefore.next;
+			 Node nodeAfter = nodeToRemove.next;
+			 nodeBefore.next = nodeAfter;
+			 nodeToRemove = null;
+		 }
+	 }
+	 
+	 //obj: prints the list
+	 // args: none
+	 // return value : void;
+	 // time complexity: (O)n;
 	 public void printList()
 	 {
 		 Node tracker = head;
