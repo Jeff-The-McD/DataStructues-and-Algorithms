de8363cf3b50dec679cf44d2aaba28faccae2ce2
Jeffrey McDonald <jmcdonald@cpp.edu>
2020-08-18 15:35:50 -0700

    "optimized maxSubarray sum"

--- Java Algorithms/Dynamic Programming/maxSubArraySum.java
+++ Java Algorithms/Dynamic Programming/maxSubArraySum.java
@@ -1,18 +1,20 @@
 import java.io.*;
 public class maxSubArraySum
 {
+  
   static int maxSubArraySum()
   {
-    int max_so_far = a[0];
-    int curr_max = a[0];
-    
-    for(int i =1;i<size;i++)
+    int max_so_far = 0, max_ending_here=0;
+    for(int i =0;i<size;i++)
     {
-      curr_max = Math.max(a[i],curr_max+a[i]);
-      max_so_far = Math.max(max_so_far,curr_max);
+          max_ending_here = max_ending_here + a[i];
+      
+          if(max_ending_here<0)
+            max_ending_here=0;
+      
+          else if(max_so_far<max_ending_here)
+            max_so_far = max_ending_here;
     }
-    return max_so_far
+    return max_so_far;
   }
-  
-
 }
