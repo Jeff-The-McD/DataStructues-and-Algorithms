5fa1a41ac7f413033fcb5726780e7a19666cae1e
Jeffrey McDonald <jmcdonald@cpp.edu>
2020-08-22 22:49:38 -0700

    Update BinarySearchOne.py

--- Python Algorithms/BinarySearchOne.py
+++ Python Algorithms/BinarySearchOne.py
@@ -19,3 +19,17 @@ def binarySearch(nums, target):
 
     # End Condition: left > right
     return -1
+"""
+Info grabbed from leetcode
+Most standard Binary Search
+Search condition can be determined without comparing to the element's neighbors(or use specific elements
+around it)
+No post-processing required because at each step, you are checking to see if the element has 
+been found. If you reach the end, then you know the element is not found.
+Distinguishing Syntax:
+Inital Condition: left =0,right=length-1
+Termination: left>right
+Searching Left: right = mid-1
+Searching Right: left = mid+1
+
+"""
