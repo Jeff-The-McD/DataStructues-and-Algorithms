216d843ce156f21f9ddafd65c421d9d5d5cb02af
Jeffrey McDonald <jmcdonald@cpp.edu>
2020-08-24 11:22:20 -0700

    Update BinarySearchThree.cpp

--- C++ Algorithms/Searching/BinarySearchThree.cpp
+++ C++ Algorithms/Searching/BinarySearchThree.cpp
@@ -21,3 +21,25 @@ int binarySearch(vector<int>& nums, int target){
     if(nums[right] == target) return right;
     return -1;
 }
+
+/* 
+Key attributes 
+An alternative way to implement Binary Search
+
+Search Condition needs to access element's immediate left and right neighbors
+
+Use element's neighbors to determine if condition is met and decide whether to go left or
+right 
+
+Gurantees Search Space is at least in size at each step
+
+Post-processing required. Loop/Recursion ends when you 2 elements left. Need to assess 
+if the remaining elements meet the condition.
+
+Distinguishing Syntax
+Initial Condition : left= 0, right = length-1
+Termination : left + 1 == right
+Searching Left : right = mid
+Searching Right : left = mid
+
+*/
