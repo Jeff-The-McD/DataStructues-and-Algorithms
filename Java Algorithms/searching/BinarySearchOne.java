3d01a0a0118130252904813e21955b18dae334e3
Jeffrey McDonald <jmcdonald@cpp.edu>
2020-10-13 23:20:50 -0700

    Update BinarySearchOne.java

--- Java Algorithms/searching/BinarySearchOne.java
+++ Java Algorithms/searching/BinarySearchOne.java
@@ -1,14 +1,16 @@
-int binarySearch(int[]nums,int target){
+int binarySearch(int[]nums,int target)
+{
   if(nums==null || nums.length==0)
     return -1;
   
   int left = 0, right = nums.length-1;
   
-  while(left<=right){
+  while(left<=right)
+  {
     int mid = left + (right-left) /2;
-  if(nums[mid]==target){return mid;}
-  else if(nums[mid]<target){left=mid+1;}
-  else{right = mid- 1;}
+    if(nums[mid]==target){return mid;}
+    else if(nums[mid]<target){left=mid+1;}
+    else{right = mid- 1;}
   }
   
   return -1;
